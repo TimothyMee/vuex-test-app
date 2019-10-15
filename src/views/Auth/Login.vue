0f3f4292f26d4a5e38e8184df6db16d3e6afe3e0
TimothyMee <timothy33.tf@gmail.com>
2019-10-15 11:19:49 +0100

    writing routes

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>{{pageName}} - {{description}}</h1>
+
+    <div>
+      <form onSubmit.prevent>
+        <div class="col-md-6">
+          <input type="email" name="email" v-model="user.email" class="form-control" />
+          <br />
+          <input type="password" name="password" v-model="user.password" class="form-control" />
+          <br />
+          <input type="submit" value="Login" class="btn btn-success" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      pageName: "Login",
+      description: "This is the Login Page",
+      user: {}
+    };
+  }
+};
+</script>
