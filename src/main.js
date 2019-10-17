ce042adb992ce0ed080631d2bbc27b976462e7c1
TimothyMee <timothy33.tf@gmail.com>
2019-10-17 10:49:02 +0100

    implemented vue-tables successfully

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
 import axios from "axios";
+import "./registerServiceWorker";
+import { ServerTable, ClientTable, Event } from "vue-tables-2";
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
+Vue.use(ClientTable);
 new Vue({
   router,
   store,
